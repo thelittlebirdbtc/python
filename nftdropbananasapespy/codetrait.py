1c075918d343e6a1ac41cd7810341b7fcd885a44
thelittlebirdbtc <thelittlebirdbtc@outlook.com>
2025-05-02 18:26:58 -0300

    Implement code changes to enhance functionality and improve performance

--- nftdropbananasapespy/codetrait.py
+++ nftdropbananasapespy/codetrait.py
@@ -1,14 +1,14 @@
 import csv
 
-start_id = 357
+start_id = 174
 
-with open('C:\\Users\\jhosephethierry\\Documents\\Bird\\python\\apespy\\apeslisthold.csv', "r") as input_file, open('C:\\Users\\jhosephethierry\\Documents\\Bird\\python\\nftdropbananasapespy\\clarCodeTrait.csv', 'w', newline='') as output_file:
+with open('C:\\Users\\jhosephethierry\\Documents\\Bird\\python\\apespy\\apeslisthold.csv', "r") as input_file, open('C:\\Users\\jhosephethierry\\Documents\\Bird\\python-1\\nftdropbananasapespy\\clarCodeTrait.csv', 'w', newline='') as output_file:
     reader = csv.reader(input_file)
     writer = csv.writer(output_file)
     for row in reader:
         addy = ",".join(row)
         current_id = f"{start_id:01d}"
-        text = f"(bananas u{current_id} '{addy}".rstrip(";")
+        text = f"(stones u{current_id} '{addy}".rstrip(";")
         linecodes = ((text) + ")")
         writer.writerow([linecodes])
         start_id += 1
